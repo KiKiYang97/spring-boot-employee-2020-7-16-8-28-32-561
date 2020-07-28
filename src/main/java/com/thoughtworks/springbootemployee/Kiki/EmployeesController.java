c66067fcee0c2216a137694f6506454a74202fb2
Dunka97 <1150311882@qq.com>
2020-07-28 23:27:15 +0800

    [Employee][KikiYang] feature : getCompaniesByPageAndPageSize

---
+++ src/main/java/com/thoughtworks/springbootemployee/Kiki/EmployeesController.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.springbootemployee.Kiki;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Dunka
+ * @Description //TODO
+ * @Date 23:17   2020/7/28
+ * @ClassName EmployeeController
+ */
+@RestController
+@RequestMapping("/Kiki/employees")
+public class EmployeesController {
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1,"Kiki",18,"female",1000d));
+        employees.add(new Employee(2,"Eason",25,"male",2000d));
+        employees.add(new Employee(3,"Eva",18,"female",3000d));
+        return employees;
+    }
+}
