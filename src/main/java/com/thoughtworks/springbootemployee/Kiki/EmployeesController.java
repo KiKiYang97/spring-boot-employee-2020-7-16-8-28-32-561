68432739744cd10e9bf7a82239b1dea57cedfcb9
Dunka97 <1150311882@qq.com>
2020-07-28 23:42:34 +0800

    [Employee][KikiYang] feature : add getEmployeesByGender

--- src/main/java/com/thoughtworks/springbootemployee/Kiki/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/Kiki/EmployeesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author Dunka
@@ -19,25 +20,26 @@ import java.util.List;
 @RequestMapping("/Kiki/employees")
 public class EmployeesController {
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1,"Kiki",18,"female",1000d));
-        employees.add(new Employee(2,"Eason",25,"male",2000d));
-        employees.add(new Employee(3,"Eva",18,"female",3000d));
+        employees.add(new Employee(1, "Kiki", 18, "female", 1000d));
+        employees.add(new Employee(2, "Eason", 25, "male", 2000d));
+        employees.add(new Employee(3, "Eva", 18, "female", 3000d));
         return employees;
     }
+
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable int id){
-        return  new Employee(id,"Kiki",18,"female",1000d);
+    public Employee getEmployeeById(@PathVariable int id) {
+        return new Employee(id, "Kiki", 18, "female", 1000d);
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Employee> getEmployeesByPageAndPageSize(int page, int pageSize) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(0, "alibaba3", 19, "male", 8000));
-        employees.add(new Employee(1,"Kiki",18,"female",1000d));
-        employees.add(new Employee(2,"Eason",25,"male",2000d));
-        employees.add(new Employee(3,"Eva",18,"female",3000d));
+        employees.add(new Employee(1, "Kiki", 18, "female", 1000d));
+        employees.add(new Employee(2, "Eason", 25, "male", 2000d));
+        employees.add(new Employee(3, "Eva", 18, "female", 3000d));
         employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
         employees.add(new Employee(5, "tengxun2", 19, "female", 7000));
         int beginNumber = (page - 1) * pageSize;
@@ -47,6 +49,23 @@ public class EmployeesController {
             employeesResult.add(employees.get(i));
         }
         return employeesResult;
+    }
+
+    @GetMapping(params = {"gender"})
+    public List<Employee> getEmployeesByGender(String gender) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(0, "alibaba3", 19, "male", 8000));
+        employees.add(new Employee(1, "Kiki", 18, "female", 1000d));
+        employees.add(new Employee(2, "Eason", 25, "male", 2000d));
+        employees.add(new Employee(3, "Eva", 18, "female", 3000d));
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(5, "tengxun2", 19, "female", 7000));
+
+        final List<Employee> collect = employees
+                .stream()
+                .filter(employee -> employee.getGender().equals(gender))
+                .collect(Collectors.toList());
+        return collect;
     }
 
 
